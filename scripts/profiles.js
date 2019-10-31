dc92ba3429bb1ed9306f09cf96a61f6d983226da
Vishal Patel <patelvishal3199@gmail.com>
2019-10-31 18:29:44 -0400

    Writes profiles to an already handled format

--- scripts/profiles.js
+++ scripts/profiles.js
@@ -19,6 +19,10 @@ const WARNING_DIRECTIVES = {
  */
 
 /**
+ * @typedef {Object.<string,string>} OldCandidateProfile
+ */
+
+/**
  * Converts a profile text file to a profile-data object.
  *
  * The file should have it's first line dedicated to CSV data that specifies
@@ -81,4 +85,23 @@ var srcDir = './profile_data/unparsed';
   }));
 
   await fs.writeFile('includes/organized-profiles.json', JSON.stringify(profiles, null, 2));
+
+  /**
+   * @type {Object.<string,OldCandidateProfile>}
+   */
+  let profilesAsObject = {};
+
+  profiles.forEach(profile => {
+    let electionValue = profile.position + (profile.isIncumbent ? ' (Incumbent)' : '');
+
+    profilesAsObject[profile.fullName] = {
+      'Election': electionValue,
+      'Name': profile.fullName,
+      'Photo file name': profile.pictureFileBaseName,
+      'Profile Text': profile.bio,
+      'Pulled Quote': profile.quote
+    };
+  });
+
+  await fs.writeFile('includes/profiles-new.json', JSON.stringify([profilesAsObject], null, 2));
 })();
