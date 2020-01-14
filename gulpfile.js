8e8da17c4786c2a76e071ddd743f0a1655835bd2
fishnal <vpatel14@terpmail.umd.edu>
2020-01-14 12:21:25 -0500

    Changed build gulp task
    Will build candidate data and minify files

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,20 @@ var buildVotes = require('./scripts/votes');
 async function build(cb) {
   await mkdirp('./build');
 
+  await Promise.all(build$CandidateData(), build$Minifiy());
+
+  cb();
+}
+
+async function build$CandidateData() {
   let profiles = await buildProfile('./data/profiles/', 'build/profiles.json');
 
   let nameMap = new Map(profiles.map(p => [p.fullName.split(' ').pop(), p.fullName]));
   await buildVotes('./data/votes.tsv', nameMap, 'build/votes.json');
+}
 
-  cb();
+async function build$Minify() {
+  // TODO minify JS, CSS, HTML, and image files
 }
 
 function clean(cb) {
